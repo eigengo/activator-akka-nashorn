256567408e716f4bc34ec9475c5ab3a8495a1d1e
Jan Machacek <jan.machacek@gmail.com>
2014-06-03 17:51:03 +0100

    RESTful webapp

--- src/main/scala/org/eigengo/activator/nashorn/WorkflowService.scala
+++ src/main/scala/org/eigengo/activator/nashorn/WorkflowService.scala
@@ -5,23 +5,28 @@ import spray.routing.{Directives, Route}
 import akka.util.Timeout
 import java.util.concurrent.TimeUnit
 import scala.concurrent.ExecutionContext
-import spray.http.{StatusCodes, HttpEntity, HttpResponse}
+import spray.http.{ContentTypes, StatusCodes, HttpEntity, HttpResponse}
+import org.json4s.{NoTypeHints, Formats}
+import org.json4s.jackson.Serialization
 
 trait WorkflowService extends Directives {
   import WorkflowActor._
   import WorkflowInstanceActor._
   import akka.pattern.ask
+  import Serialization._
 
+  private implicit val _: Formats = Serialization.formats(NoTypeHints)
   private implicit val timeout = Timeout(5000, TimeUnit.MILLISECONDS)
+
   private def toJson(value: Any): HttpResponse = value match {
     case Started(id, instruction) =>
-      HttpResponse(entity = HttpEntity(Map("id" -> id, "instruction" -> WorkflowObjectMapper.map(instruction)).toString()))
+      HttpResponse(entity = HttpEntity(ContentTypes.`application/json`, write(Map("id" -> id, "instruction" -> WorkflowObjectMapper.map(instruction)))))
     case End(id, instruction, data) =>
-      HttpResponse(entity = HttpEntity(Map("id" -> id, "instruction" -> WorkflowObjectMapper.map(instruction), "data" -> WorkflowObjectMapper.map(data)).toString()))
+      HttpResponse(entity = HttpEntity(ContentTypes.`application/json`, write(Map("id" -> id, "instruction" -> WorkflowObjectMapper.map(instruction), "data" -> WorkflowObjectMapper.map(data)))))
     case Next(id, instruction, data) =>
-      HttpResponse(entity = HttpEntity(Map("id" -> id, "instruction" -> WorkflowObjectMapper.map(instruction), "data" -> WorkflowObjectMapper.map(data)).toString()))
+      HttpResponse(entity = HttpEntity(ContentTypes.`application/json`, write(Map("id" -> id, "instruction" -> WorkflowObjectMapper.map(instruction), "data" -> WorkflowObjectMapper.map(data)))))
     case Error(id, error, instruction, data) =>
-      HttpResponse(entity = HttpEntity(Map("id" -> id).toString()), status = StatusCodes.BadRequest)
+      HttpResponse(entity = HttpEntity(ContentTypes.`application/json`, write(Map("id" -> id).toString())), status = StatusCodes.BadRequest)
   }
 
 
